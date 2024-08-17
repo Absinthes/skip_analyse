97ef7b684eadc395dcb4904f7e12091445952e24
Absinthe. <1198727859@qq.com>
2024-08-18 01:16:59 +0800

    docs: 更新

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./docs",
+  },
   plugins: [
     react(),
     Unocss({
